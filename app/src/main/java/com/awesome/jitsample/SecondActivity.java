92c1c5d5b069ac421534ab1de76690691120c5df
Alexey Bukhantsov <a.buhansov@gmail.com>
2017-02-23 15:47:50 +0200

    inner project changes

---
+++ app/src/main/java/com/awesome/jitsample/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.awesome.jitsample;
+
+import android.util.Log;
+
+/**
+ * Created by Fileas on 23.02.17.
+ */
+
+public class SecondActivity {
+
+    public static void innerMathod() {
+        Log.d("Second activity", "inner method");
+    }
+}
